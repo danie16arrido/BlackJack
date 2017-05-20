81249a9117bc277faaf80f26a06b6b4ee347c162
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-05-20 18:59:38 +0100

    add player class and pass tests

---
+++ PlayerTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class PlayerTest {
+  Player player;
+
+  @Before
+  public void before(){
+    player = new Player("Daniel");
+  }
+
+  @Test
+  public void canCreatePlayer(){
+    assertEquals(true, player instanceof Player);
+  }
+
+  @Test
+  public void canGetName(){
+    assertEquals("Daniel", player.getName());
+  }
+
+  @Test
+  public void canGetHand(){
+
+    assertEquals(true , player.getHand() instanceof Hand);
+  }
+}
